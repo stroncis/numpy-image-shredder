84340a47b11491b4cc60979501c49c84c14efeaf
Martynas <stroncis@gmail.com>
2025-06-10 01:47:12 +0300

    Live updates on inputs change
    There's no need for "SUBMIT" button

--- app.py
+++ app.py
@@ -19,21 +19,57 @@ def run_app():
 
         with gr.Row():
             clear_button = gr.Button("Clear")
-            submit_button = gr.Button("Submit")
+            # submit_button = gr.Button("Submit")
 
-        submit_button.click(
+        input_fields = [url_input, chunk_w_input, chunk_h_input]
+
+        # submit_button.click(
+        #     fn=process_image,
+        #     inputs=input_fields,
+        #     outputs=output_image
+        # )
+
+        url_input.change(
             fn=process_image,
-            inputs=[url_input, chunk_w_input, chunk_h_input],
+            inputs=input_fields,
+            outputs=output_image
+        )
+        chunk_w_input.release(
+            fn=process_image,
+            inputs=input_fields,
+            outputs=output_image
+        )
+        chunk_h_input.release(
+            fn=process_image,
+            inputs=input_fields,
             outputs=output_image
         )
 
         def clear_inputs_outputs():
-            return DEFAULT_IMAGE_URL, DEFAULT_CHUNK_W, DEFAULT_CHUNK_H, None
+            image = process_image(
+                url=DEFAULT_IMAGE_URL,
+                chunk_w=DEFAULT_CHUNK_W,
+                chunk_h=DEFAULT_CHUNK_H
+            )
+            return DEFAULT_IMAGE_URL, DEFAULT_CHUNK_W, DEFAULT_CHUNK_H, image
 
         clear_button.click(
             fn=clear_inputs_outputs,
             inputs=None,
             outputs=[url_input, chunk_w_input, chunk_h_input, output_image]
+        )
+
+        def load_initial_image():
+            return process_image(
+                url=DEFAULT_IMAGE_URL,
+                chunk_w=DEFAULT_CHUNK_W,
+                chunk_h=DEFAULT_CHUNK_H
+            )
+
+        demo.load(
+            fn=load_initial_image,
+            inputs=None,
+            outputs=output_image
         )
 
     demo.launch()
