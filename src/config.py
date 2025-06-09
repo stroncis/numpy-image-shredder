805ecde57a4c47f1188e3bb6a6335ad79f69cea2
Martynas <stroncis@gmail.com>
2025-06-10 01:41:08 +0300

    Default image changed
    Nicer, creates effect of multiple boxes with flowers

--- src/config.py
+++ src/config.py
@@ -1,6 +1,8 @@
 
-# Image source: https://commons.wikimedia.org/wiki/File:Yellow_flowers_a.jpg
-DEFAULT_IMAGE_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Yellow_flowers_a.jpg/1544px-Yellow_flowers_a.jpg"
+# Used images licenses:
+# https://commons.wikimedia.org/wiki/File:Yellow_flowers_a.jpg - Creative Commons Attribution-Share Alike 3.0 Unported license
+# https://stockcake.com/i/vibrant-flower-collection_1061865_1157624 - Public domain
+DEFAULT_IMAGE_URL = "https://images.stockcake.com/public/2/8/c/28cbee0c-98af-4fd4-98f8-8870ba7b532c/vibrant-flower-collection-stockcake.jpg"
 
 DEFAULT_CHUNK_W = 16
 DEFAULT_CHUNK_H = 16
