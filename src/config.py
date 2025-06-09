a568f6e7ea510645262d749da13676d047a492b2
Martynas <stroncis@gmail.com>
2025-06-09 21:18:15 +0300

    Nicer result, though still visible segmentation

--- src/config.py
+++ src/config.py
@@ -2,5 +2,5 @@
 # Image source: https://commons.wikimedia.org/wiki/File:Yellow_flowers_a.jpg
 DEFAULT_IMAGE_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Yellow_flowers_a.jpg/1544px-Yellow_flowers_a.jpg"
 
-DEFAULT_CHUNK_W = 32
-DEFAULT_CHUNK_H = 32
+DEFAULT_CHUNK_W = 16
+DEFAULT_CHUNK_H = 16
